cd08bde70844b55a510684a210908fdc256ef07d
SolinkfGiordi <128148099+fGiordi@users.noreply.github.com>
2024-12-29 03:11:49 +0200

    fixed db issue of tasks not existing
    - was pointing to the wrong file

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 
-const sqlite = new Database('db.sqlite');
+const sqlite = new Database('./src/db/db.sqlite');
 export const db = drizzle(sqlite);
